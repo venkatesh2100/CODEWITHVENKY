7d13ec4951144708dbc800d419dc113296762857
venkatesh2100 <119948075+venkatesh2100@users.noreply.github.com>
2024-03-02 18:46:07 +0530

    New Repo DSA is added

---
+++ DSA/ARRAYS/demo.java
@@ -0,0 +1,8 @@
+package ARRAYS;
+
+public class demo{
+    public static void main(String[] args) {
+        System.out.println("HEllO");
+    }
+}
+
---
+++ DSA/CP/CODECHEF/demo.java
@@ -0,0 +1,7 @@
+package CP.CODECHEF;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("This is Competitive Programing Repository");
+    }
+}
