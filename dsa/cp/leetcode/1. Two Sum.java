5c9dd0ea0d375a762ea794c56f2bb74af0f25648
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-06-18 21:19:36 +0530

    React code added and Two sum

--- dsa/cp/leetcode/1. Two Sum.java
+++ dsa/cp/leetcode/1. Two Sum.java
@@ -1,18 +1,15 @@
-package CP.LEETCODE;
+//
 
 public class Easy_1TwoSum {
     public int[] twoSum(int[] nums, int target) {
-        int[] sum = new int[2];
-        String name;
-        for (int i = 0; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    sum[0] = i;
-                    sum[1] = j;
-
+                    return new int[]{i, j};
                 }
-
             }
-            return sum;
         }
+        return new int[]{}; 
     }
+
+}
