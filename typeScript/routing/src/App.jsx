b3528b8ccf7b17f7ee18a0e982c5056031d5fefa
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-07-05 23:13:35 +0530

    routing

--- typeScript/routing/src/App.jsx
+++ typeScript/routing/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Footer">Footer</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" exact element={Home} />
+          <Route path="/about" element={About} />
+          <Route path="/footer" element={Footer} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
