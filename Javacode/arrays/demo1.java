dbbb2391678099aa50a902b62988fed71e204e74
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-07-20 23:17:17 +0000

    Resol

--- Javacode/arrays/demo1.java
+++ Javacode/arrays/demo1.java
@@ -1,9 +1,12 @@
 package ARRAYS;
 
 import java.util.*;
+import java.util.function.Function;
 
 public class demo1 {
   public static void main(String[] args) {
+    
+
     System.out.println("I Love coding");
     ArrayList<Integer> al = new ArrayList<>();
     al.add(100);
@@ -11,6 +14,7 @@ public class demo1 {
     al.add(10000);
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the Number you want:");
+    System.out.println();
     int s = sc.nextInt();
     for (int i = 0; i < s; i++) {
       System.out.println(s);
