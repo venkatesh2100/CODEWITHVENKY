1f28894f05e05f253f533b3865d74a364bae8fd0
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-08-07 23:15:07 +0530

    k

--- LeetcodeDaily/Ts/RemoveElement-27.ts
+++ LeetcodeDaily/Ts/RemoveElement-27.ts
@@ -10,3 +10,15 @@ function removeElement(nums: number[], val: number): number {
   }
   return j;
 }
+
+function remove(nums: number[], val: number): number {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+  return k;
+}
