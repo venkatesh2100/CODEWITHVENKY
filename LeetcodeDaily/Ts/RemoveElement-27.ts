1f9d03a9b00db35ad362f823bb499e33f3b82d67
venkatesh2100 <venkyvenkat.iit@gmail.com>
2024-08-08 15:01:22 +0530

    prismaSchema

--- LeetcodeDaily/Ts/RemoveElement-27.ts
+++ LeetcodeDaily/Ts/RemoveElement-27.ts
@@ -1,24 +1,24 @@
-function removeElement(nums: number[], val: number): number {
-  let j = 0;
-  let i = 0;
-  while (i < nums.length) {
-    if (nums[i] !== val) {
-      nums[j] = nums[i];
-      j++;
-    }
-    i++;
-  }
-  return j;
-}
+// function removeElement(nums: number[], val: number): number {
+//   let j = 0;
+//   let i = 0;
+//   while (i < nums.length) {
+//     if (nums[i] !== val) {
+//       nums[j] = nums[i];
+//       j++;
+//     }
+//     i++;
+//   }
+//   return j;
+// }
 
-function remove(nums: number[], val: number): number {
-  let k = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== val) {
-      nums[k] = nums[i];
-      k++;
+function removeElement(nums:number[],val:number):number{
+  let k=0;
+  for(let i of nums){
+    if(val!==i){
+      nums[k++];
     }
   }
+
   return k;
 }
